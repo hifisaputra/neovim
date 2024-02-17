f8e601f0d8a6b0b78058153aa4dc596747f7d86e
Hifi Saputra <hifisaputra1@gmail.com>
2024-02-17 10:24:07 +0700

    init

---
+++ after/plugin/tailwind-sort.lua
@@ -0,0 +1,5 @@
+function ToggleTailwindSort()
+    vim.cmd.TailwindSort()
+end
+
+ToggleTailwindSort()
---
+++ lua/hifi/init.lua
@@ -0,0 +1,8 @@
+require("hifi.packer")
+require("hifi.remap")
+require("hifi.set")
+
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
