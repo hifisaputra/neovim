be8da388767c5fe1f13e2f4802e3a4ac89d4e30f
Hifi Saputra <hifisaputra1@gmail.com>
2024-03-02 09:21:04 +0700

    remove unused lines

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -186,7 +186,7 @@ _G.packer_plugins = {
     url = "https://github.com/rose-pine/neovim"
   },
   ["tailwind-sorter.nvim"] = {
-    config = { "\27LJ\2\nº\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\20on_save_pattern\1\n\0\0\v*.html\t*.js\n*.jsx\n*.tsx\v*.twig\n*.hbs\n*.php\v*.heex\f*.astro\1\0\2\14node_path\tnode\20on_save_enabled\2\nsetup\20tailwind-sorter\frequire\0" },
+    config = { "\27LJ\2\nº\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\20on_save_pattern\1\n\0\0\v*.html\t*.js\n*.jsx\n*.tsx\v*.twig\n*.hbs\n*.php\v*.heex\f*.astro\1\0\2\20on_save_enabled\2\14node_path\tnode\nsetup\20tailwind-sorter\frequire\0" },
     loaded = true,
     path = "/home/dev/.local/share/nvim/site/pack/packer/start/tailwind-sorter.nvim",
     url = "https://github.com/laytan/tailwind-sorter.nvim"
@@ -204,14 +204,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: tailwind-sorter.nvim
-time([[Config for tailwind-sorter.nvim]], true)
-try_loadstring("\27LJ\2\nº\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\20on_save_pattern\1\n\0\0\v*.html\t*.js\n*.jsx\n*.tsx\v*.twig\n*.hbs\n*.php\v*.heex\f*.astro\1\0\2\14node_path\tnode\20on_save_enabled\2\nsetup\20tailwind-sorter\frequire\0", "config", "tailwind-sorter.nvim")
-time([[Config for tailwind-sorter.nvim]], false)
 -- Config for: rose-pine
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
+-- Config for: tailwind-sorter.nvim
+time([[Config for tailwind-sorter.nvim]], true)
+try_loadstring("\27LJ\2\nº\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\4\0=\3\5\2B\0\2\1K\0\1\0\20on_save_pattern\1\n\0\0\v*.html\t*.js\n*.jsx\n*.tsx\v*.twig\n*.hbs\n*.php\v*.heex\f*.astro\1\0\2\20on_save_enabled\2\14node_path\tnode\nsetup\20tailwind-sorter\frequire\0", "config", "tailwind-sorter.nvim")
+time([[Config for tailwind-sorter.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
